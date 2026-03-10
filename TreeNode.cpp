ca2044b6557d8b52ed3acc27b80975d98ad7ed08
DooneyGit <dooneytrue@gmail.com>
2026-03-09 20:38:34 -0400

    recursive build function attempt 1

--- TreeNode.cpp
+++ TreeNode.cpp
@@ -1,4 +1,32 @@
 #include "TreeNode.hpp"
 
-TreeNode::TreeNode(double left, double right) : left(left), right(right) {};
+TreeNode::TreeNode(double left, double right) : 
+    left(left), right(right), numOfCountries(0), capacity(2), left(nullptr), right(nullptr) {
+        countries = new std::string[capacity];
+    }
+
+TreeNode::~TreeNode() {
+    delete[] countries;
+}
+
+void TreeNode::resize(int newCapacity) {
+    std::string* newArray = new std::string[newCapacity];
+
+    for(int i{0}; i < numOfCountries; i++) {
+        newArray[i] = countries[i];
+    }
+
+    delete countries[];
+    countries = newArray;
+    capacity = newCapacity;
+}
+
+void TreeNode::addCountry(std::string countryName) {
+    if(numCountries == capacity) {
+        resize(capacity * 2);
+    }
+
+    countries[numOfCountries] = countryName;
+    numOfCountries++;
+}
 
--- TreeNode.hpp
+++ TreeNode.hpp
@@ -1,7 +1,15 @@
+#include <string>
+
 class TreeNode {
-private:
+public:
     double left;
     double right;
     TreeNode* left;
     TreeNode* right;
+    std::string* countries; 
+    int numOfCountries;
+    int capacity;
+    TreeNode(double leftBound, double rightBound);
+    ~TreeNode();
+    void resize(int newCapacity);
 };
